9d1d0b6e74247b4742c60a1cb33e56e8364a72b0
idosakazi123 <idosakazi@gmail.com>
2017-08-19 14:23:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/idosakazi/firebaseupdate/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.idosakazi.firebaseupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Comment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+
+    private ListView mListView;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView)findViewById(R.id.listview);
+        database = FirebaseDatabase.getInstance();
+        myRef =  database.getReference();
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+    private void showData(DataSnapshot dataSnapshot) {
+            HashMap<String, String> td = (HashMap<String,String>) dataSnapshot.child("Foods").getValue();
+            List<String> List = new ArrayList<>();
+            List.addAll(td.keySet());
+            ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,List);
+            mListView.setAdapter(adapter);
+    }
+
+
+}
